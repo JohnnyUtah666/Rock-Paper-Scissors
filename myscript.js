545cfc00b71841eeb687b84b79b548bc6ef24caf
JohnnyUtah666 <porksloppp@gmail.com>
2022-07-20 11:03:37 -0700

    change 'winner' variable to return statements within 'playRound' function

--- myscript.js
+++ myscript.js
@@ -12,31 +12,31 @@ function getComputerChoice(array) {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == "rock" && computerSelection == "scissors") {
-        winner = "Rock beats scissors, you win!";}
+        return "Rock beats scissors, you win!";}
 
     if (playerSelection == "rock" && computerSelection == "paper") {
-        winner = "Paper beats rock, you lose!";}
+        return "Paper beats rock, you lose!";}
     
     if (playerSelection == "rock" && computerSelection == "rock") {
-        winner = "Rock on rock, tie game!";}
+        return "Rock on rock, tie game!";}
 
     if (playerSelection == "paper" && computerSelection == "rock") {
-        winner = "Paper beats rock, you win!";}
+        return "Paper beats rock, you win!";}
 
     if (playerSelection == "paper" && computerSelection == "paper") {
-        winner = "Paper on paper, tie game!";}
+        return "Paper on paper, tie game!";}
 
     if (playerSelection == "paper" && computerSelection == "scissors") {
-        winner = "Scissors beats paper, you lose!";}
+        return "Scissors beats paper, you lose!";}
 
     if (playerSelection == "scissors" && computerSelection == "rock") {
-        winner = "Rock beats scissors, you lose!";}
+        return "Rock beats scissors, you lose!";}
 
     if (playerSelection == "scissors" && computerSelection == "paper") {
-        winner = "Scissors beat paper, you win!";}
+        return "Scissors beat paper, you win!";}
 
     if (playerSelection == "scissors" && computerSelection == "scissors") {
-        winner = "Scissors on Scissors, tie game!";}
+        return "Scissors on Scissors, tie game!";}
 }
 
 console.log(getComputerChoice(choice));
