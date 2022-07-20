bcb0c39ba2d2765ade11e6e08dc876a478706875
JohnnyUtah666 <porksloppp@gmail.com>
2022-07-20 10:49:44 -0700

    complete remaining conditionals for playRound function

--- myscript.js
+++ myscript.js
@@ -19,6 +19,24 @@ function playRound(playerSelection, computerSelection) {
     
     if (playerSelection == "rock" && computerSelection == "rock") {
         winner = "Rock on rock, tie game!";}
+
+    if (playerSelection == "paper" && computerSelection == "rock") {
+        winner = "Paper beats rock, you win!";}
+
+    if (playerSelection == "paper" && computerSelection == "paper") {
+        winner = "Paper on paper, tie game!";}
+
+    if (playerSelection == "paper" && computerSelection == "scissors") {
+        winner = "Scissors beats paper, you lose!";}
+
+    if (playerSelection == "scissors" && computerSelection == "rock") {
+        winner = "Rock beats scissors, you lose!";}
+
+    if (playerSelection == "scissors" && computerSelection == "paper") {
+        winner = "Scissors beat paper, you win!";}
+
+    if (playerSelection == "scissors" && computerSelection == "scissors") {
+        winner = "Scissors on Scissors, tie game!";}
 }
 
 console.log(getComputerChoice(choice));
