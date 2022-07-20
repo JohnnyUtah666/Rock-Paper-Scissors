b3ca0d22a3a6e7abd0fb6378497fd2a52e0b1c39
JohnnyUtah666 <porksloppp@gmail.com>
2022-07-20 11:51:00 -0700

    edit conditional statements

--- myscript.js
+++ myscript.js
@@ -10,33 +10,41 @@ function getComputerChoice(array) {
     return array[Math.floor(Math.random()*choice.length)];
 }
 
+let playerSelection = prompt("Rock, Paper, or Scissors?")
+let computerSelection = getComputerChoice(choice);
+playRound(playerSelection, computerSelection);
+
+
 function playRound(playerSelection, computerSelection) {
+     
     if (playerSelection == "rock" && computerSelection == "scissors") {
-        return "Rock beats scissors, you win!";}
+        winner = "Rock beats scissors, you win!";
 
-    if (playerSelection == "rock" && computerSelection == "paper") {
-        return "Paper beats rock, you lose!";}
+     } else if (playerSelection == "rock" && computerSelection == "paper") {
+        winner = "Paper beats rock, you lose!";
     
-    if (playerSelection == "rock" && computerSelection == "rock") {
-        return "Rock on rock, tie game!";}
+     } else if (playerSelection == "rock" && computerSelection == "rock") {
+        winner = "Rock on rock, tie game!";
 
-    if (playerSelection == "paper" && computerSelection == "rock") {
-        return "Paper beats rock, you win!";}
+     } else if (playerSelection == "paper" && computerSelection == "rock") {
+        winner = "Paper beats rock, you win!";
 
-    if (playerSelection == "paper" && computerSelection == "paper") {
-        return "Paper on paper, tie game!";}
+     } else if (playerSelection == "paper" && computerSelection == "paper") {
+        winner = "Paper on paper, tie game!";
 
-    if (playerSelection == "paper" && computerSelection == "scissors") {
-        return "Scissors beats paper, you lose!";}
+     } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        winner = "Scissors beats paper, you lose!";
 
-    if (playerSelection == "scissors" && computerSelection == "rock") {
-        return "Rock beats scissors, you lose!";}
+     } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        winner = "Rock beats scissors, you lose!";
 
-    if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "Scissors beat paper, you win!";}
+     } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        winner = "Scissors beat paper, you win!";
 
-    if (playerSelection == "scissors" && computerSelection == "scissors") {
-        return "Scissors on Scissors, tie game!";}
+     } else {
+        winner = "Scissors on Scissors, tie game!";
+        return winner;
+
 }
 
-console.log(getComputerChoice(choice));
+console.log(getComputerChoice(choice))}
