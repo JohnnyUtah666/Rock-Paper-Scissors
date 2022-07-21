9fc9d7d148342bebaf110f30d78adb644543c3de
JohnnyUtah666 <porksloppp@gmail.com>
2022-07-21 11:16:46 -0700

    add return statements to playRound function

--- myscript.js
+++ myscript.js
@@ -16,36 +16,34 @@ let computerSelection = getComputerChoice(choice);
 function playRound(playerSelection, computerSelection) {
      
     if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
-        winner = "Rock beats scissors, you win!";
+       return winner = "Rock beats scissors, you win!";
 
      } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
-        winner = "Paper beats rock, you lose!";
+       return winner = "Paper beats rock, you lose!";
     
      } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "rock") {
-        winner = "Rock on rock, tie game!";
+        return winner = "Rock on rock, tie game!";
 
      } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
-        winner = "Paper beats rock, you win!";
+      return winner = "Paper beats rock, you win!";
 
      } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "paper") {
-        winner = "Paper on paper, tie game!";
+        return winner = "Paper on paper, tie game!";
 
      } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
-        winner = "Scissors beats paper, you lose!";
+        return winner = "Scissors beats paper, you lose!";
 
      } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
-        winner = "Rock beats scissors, you lose!";
+        return winner = "Rock beats scissors, you lose!";
 
      } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
-        winner = "Scissors beat paper, you win!";
+        return winner = "Scissors beat paper, you win!";
 
      } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "scissors") {
-        winner = "Scissors on Scissors, tie game!";
+        return winner = "Scissors on Scissors, tie game!";
 
      } else {
-        winner = "Invalid Input";
-     
-        return winner;
+        return winner = "Invalid Input";
 
 }
 }
